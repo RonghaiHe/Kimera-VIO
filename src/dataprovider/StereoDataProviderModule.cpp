5408910396476224fc8484fecc4a198d9be285aa
robo宇 <2075894694@qq.com>
2025-03-18 13:39:14 +0800

    Update StereoDataProviderModule.cpp

--- src/dataprovider/StereoDataProviderModule.cpp
+++ src/dataprovider/StereoDataProviderModule.cpp
@@ -69,7 +69,9 @@ StereoDataProviderModule::getInputPacket() {
         // be given in PipelineParams.
         mono_imu_sync_packet->imu_stamps_,
         mono_imu_sync_packet->imu_accgyrs_,
-        mono_imu_sync_packet->world_NavState_ext_odom_));
+        mono_imu_sync_packet->world_NavState_ext_odom_,
+        ReinitPacket(),  // 默认的重初始化包
+        mono_imu_sync_packet->relative_distance_));
   }
 
   // Push the synced messages to the Frontend's input queue
